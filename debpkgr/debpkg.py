c7e22ee85c9e0d9b637d1c462fd1996a6997f498
Matthias Dellweg <dellweg@atix.de>
2017-05-31 16:59:46 +0200

    Tolerate missing md5sums in control part when reading from file
    Signed-off-by: Matthias Dellweg <dellweg@atix.de>

--- debpkgr/debpkg.py
+++ debpkgr/debpkg.py
@@ -259,7 +259,11 @@ class DebPkg(object):
         using keyword arguments.
         """
         debpkg = debfile.DebFile(filename=path)
-        md5sums = debpkg.md5sums(encoding='utf-8')
+        # existance of md5sums in control part is optional
+        try:
+            md5sums = debpkg.md5sums(encoding='utf-8')
+        except:
+            md5sums = None
         control = debpkg.control.debcontrol().copy()
         hashes = cls.make_hashes(path)
         control.update(kwargs)
